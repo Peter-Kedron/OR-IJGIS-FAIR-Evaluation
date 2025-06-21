10a0321f8fe57e8055223dc21312901ae2a49833
Peter-Kedron <peterkedron@ucsb.edu>
2025-06-20 17:11:22 -0700

    Correct file path

--- procedure/code/clean_data.R
+++ procedure/code/clean_data.R
@@ -19,7 +19,7 @@ clean_data <- function(source_dir){
 
   # Load data, Read the raw CSV output from qualtrics--------
   dat_qualtrics <- data.frame(read.csv(file.path(here(source_dir),
-                                                 sprintf("qualtircs_screener_2025_06_19.csv")
+                                                 sprintf("qualtrics_screener_2025_06_19.csv")
                                                  ),
                                        header = TRUE)
                             )
